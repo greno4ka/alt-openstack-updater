f6ab90901092d613ef503621bf9a016ac96be2b1
Grigory Ustinov <grenka@altlinux.org>
2022-04-12 13:10:41 +0300

    Refactoring indents

--- updater.sh
+++ updater.sh
@@ -13,31 +13,28 @@ for section in "library-projects" "service-projects" "service-client-projects"; 
 done
 
 for originalModuleName in $(cat scrapped.list | cut -d" " -f1); do
-# oslo crutch
-fixedModuleName=$(sed "s/oslo-/oslo\./" <<< $originalModuleName)
-#Sisyphus names' templates
-policyName3=$(sed "s/^\(python3\?-\)\?/python3-module-/" <<< $fixedModuleName)
-openstackName=$(sed "s/^/openstack-/" <<< $fixedModuleName)
-sisyphusNames="$policyName3 $openstackName"
+    # oslo crutch
+    fixedModuleName=$(sed "s/oslo-/oslo\./" <<< $originalModuleName)
+    #Sisyphus names' templates
+    policyName3=$(sed "s/^\(python3\?-\)\?/python3-module-/" <<< $fixedModuleName)
+    openstackName=$(sed "s/^/openstack-/" <<< $fixedModuleName)
+    sisyphusNames="$policyName3 $openstackName"
 
-sisyphusVersion=""
-for possibleName in $sisyphusNames; do
-if [ -z $sisyphusVersion ]; then
-    sisyphusVersion=$(grep "$possibleName[[:space:]]" src.list \
-    | cut -f2 | cut -d"-" -f1 | cut -d":" -f2 )
-fi
-done
+    sisyphusVersion=""
+    for possibleName in $sisyphusNames; do
+        if [ -z $sisyphusVersion ]; then
+            sisyphusVersion=$(grep "$possibleName[[:space:]]" src.list \
+            | cut -f2 | cut -d"-" -f1 | cut -d":" -f2 )
+        fi
+    done
 
-upstreamVersion=$(grep "^$originalModuleName[[:space:]]" scrapped.list | cut -d" " -f3)
-
-if [ -n "$sisyphusVersion" ]; then
-    versionDiagnostics "$sisyphusVersion" "$upstreamVersion" "$originalModuleName"
-
-    if [ $? == 2 ]; then
-        updatePackage "$originalModuleName" "$sisyphusNames"
+    upstreamVersion=$(grep "^$originalModuleName[[:space:]]" scrapped.list | cut -d" " -f3)
+    if [ -n "$sisyphusVersion" ]; then
+        versionDiagnostics "$sisyphusVersion" "$upstreamVersion" "$originalModuleName"
+        if [ $? == 2 ]; then
+            updatePackage "$originalModuleName" "$sisyphusNames"
+        fi
     fi
-fi
-
 done
 
 rm -f scrapped.list
